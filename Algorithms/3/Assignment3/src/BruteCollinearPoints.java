d71fc3eba91a713d28209cc509184b957701cbdc
Aegis-Liang <algorithm_learning@163.com>
2018-04-25 09:40:11 +0800

    Finish Assignment 3 - Point.java

---
+++ Algorithms/3/Assignment3/src/BruteCollinearPoints.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class BruteCollinearPoints {
+
+    private int numSegments;
+    private LineSegment[] lineSegments;
+
+    public BruteCollinearPoints(Point[] points)
+    {
+        Arrays.sort(points);
+
+    }
+
+    public int numberOfSegments()
+    {
+
+    }
+
+    public LineSegment[] segments()
+    {}
+}
