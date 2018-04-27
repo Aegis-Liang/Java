c6bffbe95240456264acf873ab4ce6a17dadf61c
Aegis-Liang <algorithm_learning@163.com>
2018-04-27 09:48:02 +0800

    Left 5 or more points issue unsolved
    such like:
    (6000, 6000) -> (9000, 9000)
    (5000, 5000) -> (9000, 9000)
    (4000, 4000) -> (9000, 9000)
    (3000, 3000) -> (9000, 9000)
    (2000, 2000) -> (9000, 9000)
    (1000, 1000) -> (9000, 9000)

--- Algorithms/3/Assignment3/src/BruteCollinearPoints.java
+++ Algorithms/3/Assignment3/src/BruteCollinearPoints.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
-import java.util.LinkedList;
+//import java.util.LinkedList;
+import edu.princeton.cs.algs4.ResizingArrayStack;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdOut;
@@ -7,66 +8,75 @@ import edu.princeton.cs.algs4.StdOut;
 public class BruteCollinearPoints {
 
     private int numLineSegments;
-    private LinkedList<LineSegment> llistLineSegment= new LinkedList<>();
+    //    private LinkedList<LineSegment> llistLineSegment= new LinkedList<>();
+    private ResizingArrayStack<LineSegment> resLineSegment = new ResizingArrayStack<>();
     private LineSegment[] lineSegments;
 
     public BruteCollinearPoints(Point[] points) {
+
+        if(points == null) throw new java.lang.IllegalArgumentException("Point array is null.");
+        for(Point ls : points)
+            if (ls == null) throw new java.lang.IllegalArgumentException("There is a point is null.");
+
         this.numLineSegments = 0;
+        Point[] copyPoints =  points.clone();
 
+        Arrays.sort(copyPoints);
+        for (int a=0; a<copyPoints.length-1;a++)
+            if(copyPoints[a].compareTo(copyPoints[a+1]) == 0) throw new java.lang.IllegalArgumentException("Repeat points");
 
-        Arrays.sort(points);
-        int length = points.length;
+        int length = copyPoints.length;
         for (int i = 0; i < length - 3; i++) {
-            Point pointI = points[i];
+            Point pointI = copyPoints[i];
             for (int j = i + 1; j < length - 2; j++) {
-                Point pointJ = points[j];
+                Point pointJ = copyPoints[j];
                 double slopeJ = pointI.slopeTo(pointJ);
                 for (int k = j + 1; k < length - 1; k++) {
-                    Point pointK = points[k];
+                    Point pointK = copyPoints[k];
                     double slopeK = pointI.slopeTo(pointK);
                     if (slopeK != slopeJ)
                         continue;
                     for (int l = k + 1; l < length; l++) {
-                        Point pointL = points[l];
+                        Point pointL = copyPoints[l];
                         double slopeL = pointI.slopeTo(pointL);
                         if (slopeL == slopeJ) {
-                            this.llistLineSegment.add(new LineSegment(pointI, pointL));
+//                            this.llistLineSegment.add(new LineSegment(pointI, pointL));
+                            this.resLineSegment.push(new LineSegment(pointI, pointL));
                             this.numLineSegments++;
                         }
                     }
                 }
             }
         }
-
-        this.lineSegments = new LineSegment[this.numLineSegments];
-        for (int i = 0; i < this.numLineSegments; i++)
-            this.lineSegments[i] = this.llistLineSegment.pop();
-
     }
 
-    public int numberOfSegments()
-    {
+    public int numberOfSegments() {
         return this.numLineSegments;
     }
 
-    public LineSegment[] segments()
-    {
+    public LineSegment[] segments() {
 //        this.lineSegments = new LineSegment[this.numLineSegments];
 //        for(int i=0; i<this.numLineSegments;i++)
 //            this.lineSegments[i] = this.llistLineSegment.pop();
-        return this.lineSegments;
+//        return this.lineSegments;
 
 //        LineSegment[] result = new LineSegment[this.numLineSegments];
 //        for(int i=0; i<this.numLineSegments;i++)
 //            result[i] = this.llistLineSegment.pop();
 //        return result;
+
+        LineSegment[] result = new LineSegment[this.numLineSegments];
+        int count = 0;
+        for (LineSegment ls : this.resLineSegment)
+            result[count++] = ls;
+        return result;
     }
 
     public static void main(String[] args) {
 
         // read the n points from a file
 //        In in = new In(args[0]);
-        In in = new In("input8.txt");
+        In in = new In("input9.txt");
         int n = in.readInt();
         Point[] points = new Point[n];
         for (int i = 0; i < n; i++) {
--- Algorithms/3/Assignment3/src/FastCollinearPoints.java
+++ Algorithms/3/Assignment3/src/FastCollinearPoints.java
@@ -3,34 +3,47 @@ import java.util.LinkedList;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.ResizingArrayStack;
 
 public class FastCollinearPoints {
     private int numLineSegments;
-    private LinkedList<LineSegment> llistLineSegment = new LinkedList<>();
+//    private LinkedList<LineSegment> llistLineSegment = new LinkedList<>();
+    private ResizingArrayStack<LineSegment> resLineSegment = new ResizingArrayStack<>();
     private LineSegment[] lineSegments;
 
     public FastCollinearPoints(Point[] points) {
+        if(points == null) throw new java.lang.IllegalArgumentException("Point array is null.");
+        for(Point ls : points)
+            if (ls == null) throw new java.lang.IllegalArgumentException("There is a point is null.");
+
+        this.numLineSegments = 0;
+        Point[] copyPoints =  points.clone();
+
         this.numLineSegments = 0;
 
-        Arrays.sort(points);
+        Arrays.sort(copyPoints);
+
+        for (int a=0; a<copyPoints.length-1;a++)
+            if(copyPoints[a].compareTo(copyPoints[a+1]) == 0) throw new java.lang.IllegalArgumentException("Repeat points");
+
         int length = points.length;
         // origin point
         for (int i = 0; i < length - 3; i++) {
-            Point pointI = points[i];
+            Point pointI = copyPoints[i];
 
-            Point[] slopes = Arrays.copyOfRange(points, i + 1, length - 1);
+            Point[] slopes = Arrays.copyOfRange(copyPoints, i + 1, length);
             Arrays.sort(slopes, pointI.slopeOrder());
 
             // start point
             for (int j = 0; j < slopes.length; j++) {
                 int count = 0;
-                Point pointJ = points[i + 1 + j];
+                Point pointJ = slopes[j];
                 double slopeJ = pointI.slopeTo(pointJ);
 
                 int cursor = j + 1;
                 while (cursor < slopes.length) {
                     // slope equals pointJ
-                    Point pointCursor = points[i + 1 + cursor];
+                    Point pointCursor = slopes[cursor];
                     if (pointI.slopeTo(pointCursor) == slopeJ) {
                         count++;
                         cursor++;
@@ -38,10 +51,19 @@ public class FastCollinearPoints {
                         break;
                 }
                 // add to link list
-                if (count >= 3) {
-                    Point pointPrevious = points[i + 1 + cursor - 1];
-                    this.llistLineSegment.add(new LineSegment(pointI, pointPrevious));
+                if (count >= 2) {
+                    Point pointPrevious = slopes[cursor - 1];
+                    this.resLineSegment.push(new LineSegment(pointI, pointPrevious));
+                    this.numLineSegments++;
+
+//                    this.llistLineSegment.add(new LineSegment(pointI, slopes[j]));
+//                    this.numLineSegments++;
+//                    for (int z = 0; z < count; z++) {
+//                        this.llistLineSegment.add(new LineSegment(slopes[z + j], slopes[z + j + 1]));
+//                        this.numLineSegments++;
+//                    }
                 }
+                j += count;
             }
         }
     }
@@ -51,17 +73,23 @@ public class FastCollinearPoints {
     }
 
     public LineSegment[] segments() {
-        this.lineSegments = new LineSegment[this.numLineSegments];
-        for (int i = 0; i < this.numLineSegments; i++)
-            this.lineSegments[i] = this.llistLineSegment.pop();
-        return this.lineSegments;
+//        this.lineSegments = new LineSegment[this.numLineSegments];
+//        for (int i = 0; i < this.numLineSegments; i++)
+//            this.lineSegments[i] = this.resLineSegment.pop();
+//        return this.lineSegments;
+
+        LineSegment[] result = new LineSegment[this.numLineSegments];
+        int count = 0;
+        for (LineSegment ls : this.resLineSegment)
+            result[count++] = ls;
+        return result;
     }
 
     public static void main(String[] args) {
 
         // read the n points from a file
 //        In in = new In(args[0]);
-        In in = new In("input8.txt");
+        In in = new In("input9.txt");
         int n = in.readInt();
         Point[] points = new Point[n];
         for (int i = 0; i < n; i++) {
